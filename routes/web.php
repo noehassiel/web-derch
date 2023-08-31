1acc01379af74cf2aa256d5b19848c431271de9d
Noé Hassiel Monterrosas Bandala <hassiel-monterrosas@hotmail.com>
2023-08-31 09:06:26 -0600

    Se genero ruta

--- routes/web.php
+++ routes/web.php
@@ -143,6 +143,16 @@ Route::get('/', [
     'as' => 'index',
 ]);
 
+
+Route::get('/noe_hassiel', [
+
+    'uses' => 'FrontController@examen',
+    'as' => 'examen',
+
+]);
+
+
+
 Route::get('/vacantes', [
     'uses' => 'FrontController@jobs',
     'as' => 'jobs.all',
